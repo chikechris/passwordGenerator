638951ba46d71ab226ff0352e583e90a7e307980
chikechris <cchike2017@gmail.com>
2020-03-08 13:36:36 -0400

    added bootstrsp, created header component

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './header/Header'
+
+const Main = () => {
+  return (
+    <>
+      <Header/>
+      <h3>Display Component</h3>
+    </>
+  )
+}
+
+export default Main
